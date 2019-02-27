835d2b0f3dc68fde4562aaa94db014f685a0a2da
kevin <kevinchannon@gmail.com>
2019-02-27 22:17:41 +0000

    Starting to read info from JSON dump
    Starting to read info from JSON dump
    
    Starting to read info from JSON dump

---
+++ components/GooglePlusImporter/locale/ossn.en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin
+ * Date: 27/02/2019
+ * Time: 20:32
+ */
+$en = array(
+    'hello:world' => 'Hello World!',
+);
+
+ossn_register_languages('en', $en);
