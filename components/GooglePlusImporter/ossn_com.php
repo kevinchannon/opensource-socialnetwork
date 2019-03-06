cfc74617761bc4cd4c8a14735dc449a45c36ac21
kevin <kevinchannon@gmail.com>
2019-03-06 22:02:54 +0000

    Finishing first draft of translation of JSON into internal objects

--- components/GooglePlusImporter/ossn_com.php
+++ components/GooglePlusImporter/ossn_com.php
@@ -9,22 +9,44 @@
  * @link      https://www.opensource-socialnetwork.org/
  */
 
+///////////////////////////////////////////////////////////////////////////////
+
 define('__OSSN_GOOGLE_PLUS_IMPORTER__', ossn_route()->com . 'GooglePlusImporter/');
+
+///////////////////////////////////////////////////////////////////////////////
 
 function google_plus_importer(){
     //Create a new page to put the G+ uploader  dialogs on.
     ossn_register_page('google_plus_importer', 'google_plus_importer_page');
 }
 
-function add_media_to_post($post, $media_json) {
-    foreach( $media_json as $medium_json ) {
-        $medium = create_medium_from_json_value($media_json["contentType"]);
+///////////////////////////////////////////////////////////////////////////////
 
-        // TODO: Add the rest of the code to read in the medium.
+function add_media_to_post(GooglePlusPost $post, array $media_json) {
+    foreach( $media_json as $medium_json ) {
+        $medium = create_medium_from_json_value($medium_json["contentType"]);
+
+        $medium->set_path($media_json["localFilePath"]);
+
+        if ( array_key_exists("comments", $medium_json) ) {
+            $comments_json = $medium_json["comments"];
+            foreach ( $comments_json as $comment_json ) {
+                $comment = new GooglePlusComment();
+                $comment->set_date($comment_json["creationTime"]);
+                $comment->set_author($comments_json["author"]["displayName"]);
+                $comment->set_content($comment_json["content"]);
+
+                $medium->add_comment($comment);
+            }
+        }
+
+        $post->add_media_item($medium);
     }
 }
 
-function add_comments_to_post($post, $comments_json) {
+///////////////////////////////////////////////////////////////////////////////
+
+function add_comments_to_post(GooglePlusPost $post, array $comments_json) {
     foreach ( $comments_json as $comment_json ) {
         $comment = new GooglePlusComment();
         $comment->set_date($comment_json["creationTime"]);
@@ -35,12 +57,69 @@ function add_comments_to_post($post, $comments_json) {
     }
 }
 
+///////////////////////////////////////////////////////////////////////////////
+
+function get_post_from_file(string $file_path) : GooglePlusPost {
+    $file_contents = file_get_contents($file_path);
+    $post_json = json_decode($file_contents, true);
+
+    $post = new GooglePlusPost();
+
+    foreach($post_json as $thing => $data) {
+        switch ( $thing ) {
+            case "creationTime" :
+                $post->set_date($data);
+                break;
+            case "author" :
+                // The author is a dictionary; we just want the display name.
+                $post->set_author($data["displayName"]);
+                break;
+            case "comments" :
+                add_comments_to_post($post, $data);
+                break;
+            case "album":
+                // An album is a dictionary with only a single thing in it: the "media" item.
+                add_media_to_post($post, $data["media"]);
+                break;
+        }
+    }
+
+    return $post;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+function add_post_to_db(GooglePlusPost $post) {
+
+    // this mapping is because there are already some users in the site DB, so
+    // we need to use their user names and not the ones from Google+.
+    $user_mapping = array(
+        "Kevin Channon" => "kevin",
+        "Joanne Channon" => "joanne",
+        "Anthony James" => "Anthony",
+        "Jackie Channon" => "jackie",
+        "Jessica Channon" => "jessica",
+    );
+
+    if ( array_key_exists($post->get_author(), $user_mapping) == false) {
+        // This is a user that we haven't seen before. Add to mapping.
+        $user_mapping[$post->get_author()] = $post->get_author();
+
+        // Add the user to the DB.
+        // TODO: Actually work out how to do things with the main DB...
+    }
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
 function google_plus_importer_page(){
 
     $takeout_dir_path = "../GooglePlusData";
 
-    $posts = array();
-
+    //
+    // Read all the posts from the files and push them into the DB.
+    //
     $posts_dir_path = $takeout_dir_path."/Takeout/Google+ Stream/Posts";
     $post_file_infos = new DirectoryIterator(dirname($posts_dir_path));
     foreach ($post_file_infos as $file_info) {
@@ -48,32 +127,14 @@ function google_plus_importer_page(){
             continue;
         }
 
-        $post_file_path = $file_info->getFilename();
-        $file_contents = file_get_contents($post_file_path);
-        $post_json = json_decode($file_contents, true);
+        $post = get_post_from_file($file_info->getFilename());
 
-        $post = new GooglePlusPost();
-
-        foreach($post_json as $thing => $data) {
-            // Find the post creation date, the content, pictures and comments (including on pictures)
-
-            switch ( $thing ) {
-                case "creationTime" :
-                    $post->set_date($data);
-                    break;
-                case "author" :
-                    // The author is a dictionary; we just want the display name.
-                    $post->set_author($data["displayName"]);
-                    break;
-                case "comments" :
-                    add_comments_to_post($post, $data);
-                    break;
-                case "album":
-                    // TODO: Actually add the code to call the add_media_to_post function...
-                    break;
-            }
-        }
+        add_post_to_db($post);
     }
 }
 
+///////////////////////////////////////////////////////////////////////////////
+
 ossn_register_callback('ossn', 'init', 'google_plus_importer');
+
+///////////////////////////////////////////////////////////////////////////////
